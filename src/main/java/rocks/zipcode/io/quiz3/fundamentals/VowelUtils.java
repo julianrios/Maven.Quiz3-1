9e79e98eb4775be94b9cfb3bdee43ed1793a840d
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-03-25 10:52:47 -0400

    passed all tests for vowelutils

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -4,20 +4,35 @@ package rocks.zipcode.io.quiz3.fundamentals;
  * @author leon on 09/12/2018.
  */
 public class VowelUtils {
+    private static final Character[] VOWELS = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o' ,'u'};
+
     public static Boolean hasVowels(String word) {
-        return null;
+        char[] letters = word.toCharArray();
+        for(Character letter : letters) {
+            for(Character vowel : VOWELS)
+            if(letter.equals(vowel)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        char[] letters = word.toCharArray();
+        for(int i = 0; i < letters.length; i++) {
+            if(isVowel(letters[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        return isVowel(word.charAt(0));
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        return hasVowels(character.toString());
     }
 }
