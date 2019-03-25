f2e047d698b72aa9aefb58667c1f4103baececf4
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-03-25 11:35:04 -0400

    passed most test for pig latin need to refactor

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -1,10 +1,16 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
     public String translate(String str) {
-        return null;
+        Pattern firstVowelPattern = Pattern.compile("^([aeiouAEIOU])");
+        Matcher matcher = firstVowelPattern.matcher(str);
+        if (matcher.find()) return str + "way";
+        return str.replaceAll("^([^aeiouAEIOU]*)(.+)", "$2$1ay");
     }
 }
